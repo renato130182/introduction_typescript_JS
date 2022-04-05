ea84d22a12bc9b85094991b4e7ba86805920076e
Renato Soares <renato130182@gmail.com>
2022-04-05 13:58:02 -0300

    interfaces

--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-console.log("hello word typescript")
+console.log("hello word typescript");
+
+function sum(a:number, b:number){ // vai validar a entrada durate o desenvolvimento
+    return a + b;
+}
+
+sum(1,3);
+
+//interfaces
+interface IAnimal{
+    name: string;
+    type: 'terreste' | 'Marinho',
+    rugir(alturaDB: number): void;
+}
+
+ const animal: IAnimal = {
+     name:'teste',
+     type:"Marinho",
+     rugir: (altuaDB) => (`${altuaDB}dB`)
+ }
+ console.log(animal);
+
+ interface Ifelino extends IAnimal  {
+     visaoNoturna: boolean;
+ }
+
+ const Felino: Ifelino = {
+     name: 'felino',
+     type: "terreste",
+     visaoNoturna: true,
+     rugir: (altuaDB) => (`${altuaDB}dB`)
+ }
+ console.log(Felino);
+ console.log(Felino.rugir(1));
+//types
